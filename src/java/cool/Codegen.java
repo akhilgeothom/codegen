87c34b0227fa3b079252ecec56b145dbdc6569f9
akhilgeothom <akhilgeothom@gmail.com>
2019-11-17 18:39:32 +0530

    built-in functions

--- src/java/cool/Codegen.java
+++ src/java/cool/Codegen.java
@@ -1,20 +1,33 @@
 package cool;
 
 import java.io.PrintWriter;
+import java.util.*;
 
 public class Codegen{
+    private String globOut;
+
+    // private Integer varCnt;
+    // private Integer labCnt;
+    // private Integer globCnt;
+
+    private HashMap<String,String> clNames = new HashMap<String,String>();
+
+    private ArrayList<String> baseFns = new ArrayList<String>();
+
+    private HashMap<String,ArrayList<AST.attr>> classAttrs = new HashMap<String,ArrayList<AST.attr>>();
+
     public Codegen(AST.program program, PrintWriter out){
         // out.println("; I am a comment in LLVM-IR. Feel free to remove me.");
 
         out.println("; ModuleID = \'"+program.classes.get(0).filename+"\'");
         out.println("source_filename = \""+program.classes.get(0).filename+"\"");
-        out.println("target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"")
+        out.println("target datalayout = \"e-m:e-i64:64-f80:128-n8:16:32:64-S128\"");
         out.println("target triple = \"x86_64-pc-linux-gnu\"\n");
 
-        progOut = "";
+        // progOut = "";
         //Write Code generator code here
-        className = "";
-        indent = "  ";
+        // className = "";
+        // indent = "  ";
 
         globOut = "";
         globOut += "; Global Constants\n";
@@ -22,22 +35,146 @@ public class Codegen{
         globOut += "@sStr = private constant [2 x i8] c\"%s\"\n";
         globOut += "@nullStr = private unnamed_addr constant [1 x i8] zeroinitializer\n";
 
-        varCnt = 0;
-        labCnt = 0;
-        globCnt = 0;
+        // varCnt = 0;
+        // labCnt = 0;
+        // globCnt = 0;
         
-        clNames = new HashMap<String,String>();
         clNames.put("Int","i32");
         clNames.put("String","i8*");
         clNames.put("Bool","i8");
 
-        baseFns = new ArrayList<String>();
-
-
-        classAttrs = new HashMap<String,ArrayList<AST.attr>>();
 
 
 
+        builtInFns(out);
+        out.println("\n");
 
+        for(AST.class_ cl: program.classes) {
+            out.println("%class."+cl.name+" = type { i8,");
+            
+            clNames.put(cl.name,"%class."+cl.name);
+            ArrayList<AST.attr> attrs = new ArrayList<AST.attr>();
+
+            // for(Map.Entry<String,AST.attr> entry: Semantic.inheritance.GetClassAttrs(cl.name).entrySet()) {
+            //     if(entry.getValue().name.equals("self"))
+            //         continue;
+
+            //     attrs.add(entry.getValue());
+            //     String tp = clNames.get(entry.getValue().typeid);
+            //     if(tp == null)
+            //         tp = "%class."+entry.getValue().typeid;
+            //     out.println(" "+tp+",");
+            // }
+            // Codegen.progOut = Codegen.progOut.substring(0,Codegen.progOut.length()-1);
+            out.println(" }\n");
+            classAttrs.put(cl.name,attrs);
+        }
+
+
+    }
+
+    public String getMangledName(String className,AST.method md) {
+        String temp = "";
+        temp += "_CN";
+        String funcName = md.name;
+        temp += (Integer.toString(className.length()));
+        temp += "_";
+        temp += (className);
+        temp += ("_FN");
+        temp += (Integer.toString(funcName.length()));
+        temp += (funcName);
+        temp += ("_AL");
+        temp += (Integer.toString(md.formals.size()));
+        if(md.formals.size() == 0)
+            temp += ("_NP_");
+        else
+        {
+            for(int j = 0; j < md.formals.size(); j++)
+            {
+                temp += (Integer.toString(j));
+                temp += ("N");
+                temp += (Integer.toString(md.formals.get(j).typeid.length()));
+                temp.concat(md.formals.get(j).typeid);
+            }
+        }
+
+        return temp;
+    }
+
+    void builtInFns(PrintWriter out) {
+        globOut += "; Global Constants\n";
+        globOut += "@dStr = private constant [2 x i8] c\"%d\"\n";
+        globOut += "@sStr = private constant [2 x i8] c\"%s\"\n";
+        globOut += "@nullStr = private unnamed_addr constant [1 x i8] zeroinitializer\n";
+
+        out.println("; C function\n");
+        out.println("declare void @exit(i32)\n");
+        out.println("; C function\n");
+        out.println("declare i8* @malloc(i64)\n");
+        out.println("; C function\n");
+        out.println("declare i32 @printf(i8* , ...)\n");
+        out.println("; C function\n");
+        out.println("declare i32 @scanf(i8* , ..).)\n");
+        out.println("; C function\n");
+        out.println("declare i32 @strlen(i8*)\n");
+        out.println("; C function\n");
+        out.println("declare i8* @strcat(i8*, i8*)\n");
+        out.println("; C function\n");
+        out.println("declare i8* @strcpy(i8*, i8*)\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("abort");
+        out.println("define void @abort(i32 %a1) {\n");
+        out.println("call void @exit(i32 %a1)\n");
+        out.println("ret void\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("out_string");
+        out.println("define void @out_string(i8* %a1) {\n");
+        out.println("call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([2 x i8], [2 x i8]* @sStr, i32 0, i32 0),i8* %a1)\n");
+        out.println("ret void\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("in_string");
+        out.println("define void @in_string(i8* %a1) {\n");
+        out.println("call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([2 x i8], [2 x i8]* @sStr, i32 0, i32 0),i8* %a1)\n");
+        out.println("ret void\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("out_int");
+        out.println("define void @out_int(i32 %a1) {\n");
+        out.println("call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([2 x i8], [2 x i8]* @dStr, i32 0, i32 0),i32 %a1)\n");
+        out.println("ret void\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("in_int");
+        out.println("define i32 @in_int() {\n");
+        out.println("%v1 = alloca i32\n");
+        out.println("call i32 (i8*, ...) @scanf(i8* getelementptr inbounds ([2 x i8], [2 x i8]* @sStr, i32 0, i32 0),i32* %v1)\n");
+        out.println("%v2 = load i32, i32* %v1\n");
+        out.println("ret i32 %v2\n}\n\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("length");
+        out.println("define i32 @length(i8* %a1) {\n");
+        out.println("%v1 = call i32 @strlen(i8* %a1)\n");
+        out.println("ret i32 %v1\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("concat");
+        out.println("define i8* @concat(i8* %a1, i8* %a2){\n");
+        out.println("call i8* @strcat(i8* %a1, i8* %a2)\n");
+        out.println("ret i8* %a1\n}\n\n");
+
+        out.println("; Cool Function\n");
+        baseFns.add("substr");
+        out.println("define i8* @substr(i8* %s, i32 %i, i32 %l){\n");
+        out.println("%z = zext i32 %l to i64\n");
+        out.println("%str = call noalias i8* @malloc(i64 %z)\n");
+        out.println("%ptr = getelementptr inbounds i8, i8* %s, i32 %i\n");
+        out.println("%foo = call i8* @strncpy(i8* %str, i8* %ptr, i64 %z)\n");
+        out.println("ret i8* %str\n}\n\n");
+      
+        out.println("\n");
     }
 }
--- src/java/helloworld.ll
+++ src/java/helloworld.ll
@@ -1,1 +1,62 @@
 ; I am a comment in LLVM-IR. Feel free to remove me.
+target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
+target triple = "x86_64-unknown-linux-gnu"
+@Abortdivby0 = private unnamed_addr constant [22 x i8] c"Error: Division by 0\0A\00", align 1
+@Abortdispvoid = private unnamed_addr constant [25 x i8] c"Error: Dispatch on void\0A\00", align 1
+
+declare i32 @printf(i8*, ...)
+declare i32 @scanf(i8*, ...)
+declare i32 @strcmp(i8*, i8*)
+declare i8* @strcat(i8*, i8*)
+declare i8* @strcpy(i8*, i8*)
+declare i8* @strncpy(i8*, i8*, i32)
+declare i64 @strlen(i8*)
+declare i8* @malloc(i64)
+declare void @exit(i32)
+@strformatstr = private unnamed_addr constant [3 x i8] c"%s\00", align 1
+@intformatstr = private unnamed_addr constant [3 x i8] c"%d\00", align 1
+
+%class.Object.Base = type { }
+%class.Object = type { i32, i8*, %class.Object.Base }
+
+%class.IO.Base = type {%class.Object.Base* }
+%class.IO = type { i32, i8*, %class.IO.Base }
+
+%class.Main.Base = type {%class.Object.Base* }
+%class.Main = type { i32, i8*, %class.Main.Base }
+
+@_ZTV6Object = constant [3 x i8*] [i8* bitcast (%class.Object* ( %class.Object* )* @_ZN6Object5abort to i8*), i8* bitcast ([1024 x i8]* ( %class.Object* )* @_ZN6Object9type_name to i8*), i8* bitcast (i32 ( %class.Object* )* @_ZN6Object4copy to i8*)] 
+@_ZTV2IO = constant [7 x i8*] [i8* bitcast (%class.Object* ( %class.Object* )* @_ZN6Object5abort to i8*), i8* bitcast ([1024 x i8]* ( %class.Object* )* @_ZN6Object9type_name to i8*), i8* bitcast (%class.IO* ( %class.IO* )* @_ZN2IO4copy to i8*), i8* bitcast (%class.IO* ( %class.IO*, [1024 x i8]* )* @_ZN2IO10out_string to i8*), i8* bitcast (%class.IO* ( %class.IO*, i32 )* null to i8*), i8* bitcast ([1024 x i8]* ( %class.IO* )* @_ZN2IO9in_string to i8*), i8* bitcast (i32 ( %class.IO* )* @_ZN2IO9in_int to i8*)] 
+@_ZTV4Main = constant [4 x i8*] [i8* bitcast (%class.Object* ( %class.Object* )* @_ZN6Object5abort to i8*), i8* bitcast ([1024 x i8]* ( %class.Object* )* @_ZN6Object9type_name to i8*), i8* bitcast (i32 ( %class.Object* )* @_ZN4Main4copy to i8*), i8* bitcast (%class.IO* (  )* @_ZN4Main4main to i8*)] 
+define %classObject* @_ZN6Object5abort( %class.Object* %this ) noreturn {
+entry:
+call void @exit( i32 1 )
+ret %classObject* null
+}
+
+define void @_Z6ObjectBaseC ( %class.Object.Base ) {
+entry:
+ret void
+}
+
+define [1024 x i8]* @_ZN6Object9type_name( %class.Object* %this ) {
+entry:
+%0 = getelementptr inbounds %classObject, %classObject* %this, i32 0, i32 0
+%1 = load i32, i32* %0
+%2 = getelementptr inbounds [8 x [1024 x i8]], [8 x [1024 x i8]]* @classnames, i32 0, i32 %1
+%retval = call [1024 x i8]* @_ZN6String4copy( [1024 x i8]* %2 )
+ret [1024 x i8]* %retval
+}
+
+define void @_Z2IOBaseC ( %IO.Base*%this ) { 
+entry: 
+%0 = getelementptr inbouds %class.Object.Base.Base, class.Object.Base.Base* %this, i32 0, i32 0
+call void @_Z11Object.BaseBaseC( %class.Object.Base.Base*%0
+return void
+define void @_Z4MainBaseC ( %Main.Base*%this ) { 
+entry: 
+%0 = getelementptr inbouds %class.Object.Base.Base, class.Object.Base.Base* %this, i32 0, i32 0
+call void @_Z11Object.BaseBaseC( %class.Object.Base.Base*%0
+return void
+@.str = private unnamed_addr constant [14 x i8] c"Hello world!
+\00", align 1
